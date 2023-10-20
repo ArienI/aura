db3cdeb210a6537b14bceb29e08ba376a8506a74
Arien <Laurilin2022@gmail.com>
2023-10-20 03:39:41 +0200

    Start_Building Main Page

--- app.js
+++ app.js
@@ -14,3 +14,28 @@ document.addEventListener("DOMContentLoaded", function() {
     sticker.style.top = randomTop + "px";
   });
 });
+
+// Находим кнопку по ID
+var timeButton = document.getElementById("timeButton");
+
+// Функция для обновления времени на кнопке
+function updateTime() {
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+
+  // Добавляем нули перед однозначными числами
+  hours = hours < 10 ? '0' + hours : hours;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  // Обновляем текст кнопки
+  timeButton.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+// Устанавливаем интервал для обновления времени каждую секунду
+setInterval(updateTime, 1000);
+
+// Обновляем время сразу при загрузке страницы
+updateTime();
